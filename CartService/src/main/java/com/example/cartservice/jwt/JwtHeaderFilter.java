887017a12077ec4f680fd1483019def7a8c9fae8
Roman Pribytkov <pribit97@mail.ru>
2026-04-12 17:21:12 +0500

    feat: added jwt filter in all services

---
+++ CartService/src/main/java/com/example/cartservice/jwt/JwtHeaderFilter.java
@@ -0,0 +1,34 @@
+package com.example.cartservice.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class JwtHeaderFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+        String rolesHeader = request.getHeader("X-User-Role");
+        if(rolesHeader != null){
+            List<SimpleGrantedAuthority> authorities =
+                    Arrays.stream(rolesHeader.split(","))
+                            .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                            .toList();
+            Authentication auth = new UsernamePasswordAuthenticationToken(null, null, authorities);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
+       filterChain.doFilter(request, response);
+    }
+}
--- ProductService/src/main/java/com/example/productservice/config/SecurityConfig.java
+++ ProductService/src/main/java/com/example/productservice/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.example.productservice.config;
 
+import com.example.productservice.jwt.JwtHeaderFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -15,7 +17,10 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
-                ).build();
+                        .requestMatchers("/api/products/**").hasAnyRole("USER", "ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .addFilterBefore(new JwtHeaderFilter(), UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 }
